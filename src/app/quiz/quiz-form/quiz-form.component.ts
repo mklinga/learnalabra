bd83d6698d6950ee9b7039ce9419b97f2e99a362
Markus Klinga <laite@gmx.com>
2016-12-27 17:23:18 +0100

    Reset hasbeenchecked/checkresults on new questions

--- src/app/quiz/quiz-form/quiz-form.component.ts
+++ src/app/quiz/quiz-form/quiz-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, SimpleChanges } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 
 import { AnswerMap, Guess, GuessFormMap, QuestionMap, QuestionWord } from '../../interfaces';
@@ -22,15 +22,31 @@ export class QuizFormComponent {
 
   constructor(public questionService: Questions) {}
 
+  ngOnChanges (changes: SimpleChanges) {
+    // We reset the hasBeenChecked/checkResults when we get new questions
+    this.hasBeenChecked = false;
+    this.checkResults = {};
+  }
+
   makeQuestionMap (questions): QuestionMap {
     return questions.reduce((map, question) =>
       Object.assign(map, { [question.id]: question }), {});
   }
 
   onSubmit(form) {
+
+    const done = (): void => {
+      this.answers = {};
+      this.quizDone.emit('all done here!');
+    }
+
     const guesses: GuessFormMap = form.value;
     const correctAnswers: AnswerMap = this.questionService
       .checkAnswers(this.makeQuestionMap(this.questions), guesses);
+
+    const allCorrect: boolean = Object
+      .keys(correctAnswers)
+      .reduce((result: boolean, key: string) => result && correctAnswers[key].correct, true);
 
     if (!this.hasBeenChecked) {
       const guessesToSave: Array<Guess> = Object
@@ -40,18 +56,16 @@ export class QuizFormComponent {
       this.questionService
         .saveGuessesToServer(guessesToSave)
         .subscribe(() => {
-          this.checkResults = correctAnswers;
-          this.hasBeenChecked = true;
+          if (allCorrect) {
+            done();
+          } else {
+            // The results are checked, but we still have some errors in the form
+            this.checkResults = correctAnswers;
+            this.hasBeenChecked = true;
+          }
         });
-    }
-
-    const allCorrect: boolean = Object
-      .keys(correctAnswers)
-      .reduce((result: boolean, key: string) => result && correctAnswers[key].correct, true);
-
-    if (allCorrect) {
-      this.answers = {};
-      this.quizDone.emit('all done here!');
+    } else if (allCorrect) {
+      done();
     }
   }
 }
